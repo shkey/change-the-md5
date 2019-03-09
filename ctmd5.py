a9174a78c568d2714c6cf5e9cc5d69e7a0e68ba9
shkey325 <shkey325@gmail.com>
2019-03-09 23:53:43 +0800

    初次提交

---
+++ ctmd5.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import argparse
+import hashlib
+import os
+import random
+import sys
+
+__VERSION__ = '0.1.0'
+CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='a cli tool used to change the MD5 of files',
+    )
+    parser.add_argument(
+        'file',
+        metavar='FILE',
+        type=str,
+        help='the file you want to change md5',
+    )
+    parser.add_argument(
+        '-s', '--show', action='store_true',
+        help='show the md5 value'
+    )
+    parser.add_argument('-v', '--version',
+                        action='version', version=__VERSION__)
+    return parser
+
+
+def get_md5(file):
+    if os.path.exists(file):
+        with open(file, 'rb') as f:
+            md5 = hashlib.md5()
+            while True:
+                buffer = f.read(4096)
+                if not buffer:
+                    break
+                md5.update(buffer)
+            return md5.hexdigest()
+
+
+def change_the_md5(file, show_md5=False):
+    random_str = ''.join(random.choice(CHARACTERS))
+    if show_md5:
+        print('before: {0}'.format(get_md5(file)))
+    if os.path.exists(file):
+        with open(file, 'ab') as f:
+            f.write(random_str.encode('utf-8'))
+        print('The md5 value has been changed.')
+    else:
+        raise OSError('File does not exist!')
+    if show_md5:
+        print('after: {0}'.format(get_md5(file)))
+
+def cli():
+    args = vars(get_parser().parse_args())
+    change_the_md5(file=args.get('file', None), show_md5=args.get('show', None))
+
+if __name__ == '__main__':
+    cli()
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+import ctmd5
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='ctmd5',
+    version=ctmd5.__VERSION__,
+    description='change the MD5',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='shkey',
+    author_email='shkey325@gmail.com',
+    python_requires='>=3',
+    url='https://github.com/shkey/change-the-md5',
+    keywords='MD5',
+    entry_points={
+        'console_scripts': ['ctmd5=ctmd5:cli'],
+    },
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
